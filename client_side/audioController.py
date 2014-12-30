74828affa76fec996322c2f11ffdab304870d340
Julien Heyman <bidsomail at gmail.com>
2014-12-30 15:46:22 +0100

    improved script with logging ability, config file, and launh as a daemon

--- client_side/audioController.py
+++ client_side/audioController.py
@@ -1,27 +1,89 @@
 #!/usr/bin/python
+
+# Logging and daemon instanciatiation following this:
+# 	http://blog.scphillips.com/2013/07/getting-a-python-script-to-run-in-the-background-as-a-service-on-boot/
+
 import pylirc, time
 import telnetlib
 import RPi.GPIO as GPIO
 import time
 import smbus
 import os
+import sys
+import logging
+import logging.handlers
+from ConfigParser import SafeConfigParser
 
-MAC_address = "80:1f:02:65:39:47"
-LMS_IPaddress = "192.168.0.13"
+###########################
+# PERSONAL CONFIG FILE READ
+###########################
+
+parser = SafeConfigParser()
+parser.read('audioController.ini')
+
+# Read path to log file
+LOG_FILENAME = parser.get('config', 'log_filename')
+
+MAC_address = parser.get('config', 'mac_address')
+
+LMS_IPaddress = parser.get('config', 'lms_ip_address')
+
+#################
+#  LOGGING SETUP
+#################
+LOG_LEVEL = logging.INFO  # Could be e.g. "DEBUG" or "WARNING"
+
+# Configure logging to log to a file, making a new file at midnight and keeping the last 3 day's data
+# Give the logger a unique name (good practice)
+logger = logging.getLogger(__name__)
+# Set the log level to LOG_LEVEL
+logger.setLevel(LOG_LEVEL)
+# Make a handler that writes to a file, making a new file at midnight and keeping 3 backups
+handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when="midnight", backupCount=3)
+# Format each log message like this
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+# Attach the formatter to the handler
+handler.setFormatter(formatter)
+# Attach the handler to the logger
+logger.addHandler(handler)
+
+# Make a class we can use to capture stdout and sterr in the log
+class MyLogger(object):
+	def __init__(self, logger, level):
+		"""Needs a logger and a logger level."""
+		self.logger = logger
+		self.level = level
+
+	def write(self, message):
+		# Only log if there is a message (not just a new line)
+		if message.rstrip() != "":
+			self.logger.log(self.level, message.rstrip())
+
+# Replace stdout with logging to file at INFO level
+sys.stdout = MyLogger(logger, logging.INFO)
+# Replace stderr with logging to file at ERROR level
+sys.stderr = MyLogger(logger, logging.ERROR)
+
+logger.info('Starting Audio controller service')
+logger.info('Using MAC_address %s' % MAC_address)
+logger.info("Using LMS_IPaddress %s" % LMS_IPaddress)
+
+#MAC_address = "80:1f:02:65:39:47"
+#LMS_IPaddress = "192.168.0.13"
 
 tn = telnetlib.Telnet()
 
 while(True):
 	
-	print "Starting audio controller..."
+	logger.info("Starting audio controller...")
 
-	print "Initializing remote control"
+	logger.info("Initializing remote control")
 	if(not pylirc.init("pylirc", "./conf", 1)):
-		print "Error initializing pylirc"
+		logger.info("Error initializing pylirc")
 		break	
 
 	# Configure GPIO pin used to drive the amplifier's shutdown mode
-	print "Configuring GPIO"
+	logger.info("Configuring GPIO")
 	GPIO.setwarnings(False)	
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(23, GPIO.OUT)
@@ -29,7 +91,7 @@ while(True):
 	# check that audio amplifier is up 
 	i2c_OK = False
 
-	print "Checking  i2C communication..."
+	logger.info("Checking i2C communication...")
 	GPIO.output(23, GPIO.HIGH) # make sure amp is not in shutdown mode
 
 	while (i2c_OK == False):
@@ -43,10 +105,10 @@ while(True):
 			bus.write_byte(DEVICE_ADDRESS, 0x00)
 
 			i2c_OK = True
-			print "Audio amplifier communication is OK"
+			logger.info("Audio amplifier communication is OK")
 
 		except:
-			print "Error checking i2C comm, retrying..."
+			logger.info("Error checking i2C comm, retrying...")
 			time.sleep(2)
 		
 	GPIO.output(23, GPIO.LOW) # by default, set amp back in shutdown mode
@@ -56,83 +118,83 @@ while(True):
 	# Verify that LMS server is up before continuing
 	LMS_server_OK = False
 
-	print "Checking connection to server"
+	logger.info("Checking connection to server")
 	while (LMS_server_OK == False):
 
 		response = os.system("ping -c 1 " + LMS_IPaddress)
 
 		if response == 0:
-			print "Server is up"
+			logger.info("Server is up")
 			LMS_server_OK = True
 		else:
-			print "Server is not up, waiting..."
+			logger.info("Server is not up, waiting...")
 			time.sleep(2)
 
 	try:
-		print "Entering main command loop..."
+		logger.info("Entering main command loop...")
 		while(True):
-			print "Waiting for next IR command..."
+			logger.info("Waiting for next IR command...")
 			s = pylirc.nextcode(1)
 			if s is not None:
 				cmd = s[0]["config"]
-				print "Command: %s" % cmd
+				logger.info("Command: %s", cmd)
 				repeat = s[0]["repeat"]
-				print "Repeat: %s" % repeat
+				logger.info("Repeat: %s",repeat)
 			else:
 				continue
 
 			if cmd == "play":
-				print "PLAY/PAUSE"
+				logger.info("PLAY/PAUSE")
 				tn.write(MAC_address + " pause\n")
 			elif (cmd == "mute"):
-				print "MUTE"
+				logger.info("MUTE")
 				tn.write(MAC_address + " mixer muting\n")
 			elif (cmd == "volume_plus"):
-				print "volume PLUS"
+				logger.info("volume PLUS")
 				tn.write(MAC_address + " mixer volume +5\n")
 			elif (cmd == "volume_minus"):
-				print "volume_minus"
+				logger.info("volume_minus")
 				tn.write(MAC_address + " mixer volume -5\n")
 			elif (cmd == "previous"):
-				print "PREVIOUS"
+				logger.info("PREVIOUS")
 				tn.write(MAC_address + " playlist index -1\n")
 			elif (cmd == "next"):
-				print "NEXT"
+				logger.info("NEXT")
 				tn.write(MAC_address + " playlist index +1\n")
 			elif (cmd == "button_1"):
-				print "PLAYLIST_1"
+				logger.info("PLAYLIST_1")
 				tn.write(MAC_address + " playlist play playlist_1.m3u\n")
 			elif (cmd == "button_2"):
-				print "PLAYLIST_2"
+				logger.info("PLAYLIST_2")
 				tn.write(MAC_address + " playlist play playlist_2.m3u\n")
 			elif (cmd == "button_3"):
-				print "PLAYLIST_3"
+				logger.info("PLAYLIST_3")
 				tn.write(MAC_address + " playlist play playlist_3.m3u\n")
 			elif (cmd == "button_4"):
-				print "PLAYLIST_4"
+				logger.info("PLAYLIST_4")
 				tn.write(MAC_address + " playlist play playlist_4.m3u\n")
 			elif (cmd == "button_5"):
-				print "PLAYLIST_5"
+				logger.info("PLAYLIST_5")
 				tn.write(MAC_address + " playlist play playlist_5.m3u\n")
 			elif (cmd == "button_6"):
-				print "PLAYLIST_6"
+				logger.info("PLAYLIST_6")
 				tn.write(MAC_address + " playlist play playlist_6.m3u\n")
 			elif (cmd == "button_7"):
-				print "PLAYLIST_7"
+				logger.info("PLAYLIST_7")
 				tn.write(MAC_address + " playlist play playlist_7.m3u\n")
 			elif (cmd == "button_8"):
-				print "PLAYLIST_8"
+				logger.info("PLAYLIST_8")
 				tn.write(MAC_address + " playlist play playlist_8.m3u\n")
 			elif (cmd == "button_9"):
-				print "PLAYLIST_9"
+				logger.info("PLAYLIST_9")
 				tn.write(MAC_address + " playlist play playlist_9.m3u\n")
 			elif (cmd == "power"):
 				# Now is a good time to verify that server is still up. If it's not, go back to
 				# initialization and wait for it.
-				print "[Power onf/off] Checking connection to server"
+				logger.info("[Power on/off] Checking connection to server")
 				response = os.system("ping -c 1 " + LMS_IPaddress)
 				if response != 0:
-					print "Server is down, restarting!"
+					logger.info("Server is down, restarting!")
 					pylirc.exit()
 					GPIO.cleanup()
 					break
@@ -140,8 +202,8 @@ while(True):
 				# Handle power on/off command
 				if (power == 0):
 					power = 1
-					print "power ON"
-					print "Initializing telnet connection to server"
+					logger.info("power ON")
+					logger.info("Initializing telnet connection to server")
 					if (tn is not None):
 						tn.open(LMS_IPaddress, "9090")					
 					# drive SHDN pin to HIGH to disable shutdown mode on amp, effectively turning it ON
@@ -152,7 +214,7 @@ while(True):
 					tn.write(MAC_address + " playlist play audio_on.wav\n")
 				elif (power == 1):
 					power = 0
-					print "power OFF"
+					logger.info("power OFF")
 					# Send command to LMS to play the OFF jingle
 					tn.write(MAC_address + " playlist play audio_off.wav\n")
 					# Allow for a few seconds for OFF sound to be played
@@ -165,7 +227,7 @@ while(True):
 					if (tn is not None):
 						tn.close()
 	except:
-		print "Exception in main loop, restarting audio controller"
+		logger.info("*****Exception in main loop, restarting audio controller******")
 		continue
 
 
