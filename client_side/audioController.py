509ac5160aac941c184b436c55d0af7a966a7728
Julien Heyman <bidsomail at gmail.com>
2015-04-28 22:07:17 +0200

    added pylirc exit in exception handling, to avoid error returned without exception set message

--- client_side/audioController.py
+++ client_side/audioController.py
@@ -275,6 +275,8 @@ while(True):
 		exc_type, exc_value, exc_traceback = sys.exc_info()
 		traceback.print_exception(exc_type, exc_value, exc_traceback,limit=2, file=sys.stdout)	
 		del exc_traceback
+		pylirc.exit()
+		GPIO.cleanup()
 		time.sleep(5.0)
 		continue
 
